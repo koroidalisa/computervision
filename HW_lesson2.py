5561e750028b7aec87cbddd7faf531c136abf351
Koroid Alisa <koroidalisa@gmail.com>
2025-09-29 20:04:26 +0300

    HW_lesson2.py

--- HW_lesson2.py
+++ HW_lesson2.py
@@ -1,18 +1,18 @@
 import cv2
 import numpy as np
 
-# image = cv2.imread("image/imagehw2902.jpg")
-# print(image.shape)
-# image = cv2.resize(image, (image.shape[1]//3, image.shape[0]//3))
-#
-#
-# image= cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#
-# image = cv2.Canny(image, 150, 150)
-#
-# cv2.imshow("me", image)
+image = cv2.imread("images/imagehw2902.jpg")
+print(image.shape)
+image = cv2.resize(image, (image.shape[1]//3, image.shape[0]//3))
 
-image2 = cv2.imread("image/imagehw2902_2.jpg")
+
+image= cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+image = cv2.Canny(image, 150, 150)
+
+cv2.imshow("me", image)
+
+image2 = cv2.imread("images/imagehw2902_2.jpg")
 print(image2.shape)
 image2 = cv2.resize(image2, (image2.shape[1]//2, image2.shape[0]//2))
 
@@ -26,4 +26,5 @@ cv2.imshow("me", image2)
 
 
 cv2.waitKey(0)
+
 cv2.destroyAllWindows()
