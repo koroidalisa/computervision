1f5bd3b4333aa4e240f653a8c54d772a5b6a26cc
Koroid Alisa <koroidalisa@gmail.com>
2025-11-17 22:19:03 +0200

    Add files via upload

---
+++ HW_lesson11.py
@@ -0,0 +1,40 @@
+import pandas as pd # для файлів csv
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data/figures.csv')  #робота з csv
+encoder = LabelEncoder()
+df['label_enc'] = encoder.fit_transform(df['label'])
+#df['ratio'] = df['area'] / df['perimeter']
+
+X = df[['area', 'perimeter', 'corners']] #обираэмо елементи для навчання
+y = df['label_enc']
+
+#створення моделі
+model = keras.Sequential([layers.Dense(16, activation = 'relu', input_shape = (3,)),
+                          layers.Dense(16, activation = 'relu'),
+                          layers.Dense(16, activation = 'softmax')])
+
+#навчення моделі, створення графіку
+model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+history = model.fit(X, y, epochs = 200, verbose = 0)
+plt.plot(history.history['loss'], label = 'Втрата (Loss)')
+plt.plot(history.history['accuracy'], label = 'Точність (Accuracy)')
+#plt.plot(history.history['ratio'], label = 'Відношення (Ratio)')
+
+plt.xlabel("Епоха")
+plt.ylabel('Значення')
+plt.title('Процес навчання')
+plt.legend()
+plt.show()
+
+#тестування
+test = np.array([77, 43, 6])
+
+pred = model.predict(test)
+print(f'Імовірність по кожному класу: {pred}')
+print(f'Модель визначила: {encoder.inverse_transform([np.argmax(pred)])}')
